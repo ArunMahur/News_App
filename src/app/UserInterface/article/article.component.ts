806333ef36af4f7ec6b770bec053ff980d11022a
kirti513 <kirti.khandelwal@robomq.io>
2020-03-23 11:40:38 +0530

    kk update

--- src/app/UserInterface/article/article.component.ts
+++ src/app/UserInterface/article/article.component.ts
@@ -8,22 +8,22 @@ import { NewsApiService } from '../../news-api.service';
 })
 export class ArticleComponent implements OnInit {
 
-  mArticles:Array<any>;
-  mSources:Array<any>;
+  mArticles: Array<any>;
+  mSources: Array<any>;
 
-  constructor(private newsapi:NewsApiService){
-    console.log('app component constructor called');         
+  constructor(private newsapi: NewsApiService) {
+    console.log('app component constructor called');
   }
 
   ngOnInit() {
-        //load articles
+        // load articles
       this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
-    //load news sources
-    this.newsapi.initSources().subscribe(data=> this.mSources = data['sources']);  
+    // load news sources
+      this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
     }
 
   searchArticles(source){
-    console.log("selected source is: "+source);
+    console.log('selected source is: ' + source);
     this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
   }
 }
